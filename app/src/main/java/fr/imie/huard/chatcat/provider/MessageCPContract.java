1941a4baa7431990373bddacb3d3691b617d4501
Huard Timothée <huard.tim@laposte.net>
2017-05-11 16:43:50 +0200

    modif des providers

--- app/src/main/java/fr/imie/huard/chatcat/provider/MessageCPContract.java
+++ app/src/main/java/fr/imie/huard/chatcat/provider/MessageCPContract.java
@@ -11,11 +11,13 @@ public class MessageCPContract{
     public static final String AUTHORITY = "fr.imie.huard.chatcat";
     public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + AUTHORITY);
     public static final String PATH_MESSAGE = "message";
-    public static final class PostEntry implements BaseColumns {
+    public static final class MessageEntry implements BaseColumns {
         public static final Uri CONTENT_URI =
                 BASE_CONTENT_URI.buildUpon().appendPath(PATH_MESSAGE).build();
 
-        public static final String TABLE_NAME = "messages";
+        public static final String
+
+                TABLE_NAME = "messages";
         public static final String COLUMN_PSEUDO = "pseudo";
         public static final String COLUMN_MESSAGE = "message";
         public static final String COLUMN_DATE = "date";
--- app/src/main/java/fr/imie/huard/chatcat/provider/MessageProvider.java
+++ app/src/main/java/fr/imie/huard/chatcat/provider/MessageProvider.java
@@ -1,11 +1,16 @@
 package fr.imie.huard.chatcat.provider;
 
 import android.content.ContentProvider;
+import android.content.ContentUris;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+
+import static fr.imie.huard.chatcat.provider.MessageCPContract.MessageEntry.TABLE_NAME;
 
 /**
  * Created by huard.cdi04 on 11/05/2017.
@@ -20,7 +25,27 @@ public class MessageProvider extends ContentProvider {
     @Nullable
     @Override
     public Cursor query(@NonNull Uri uri, @Nullable String[] projection, @Nullable String selection, @Nullable String[] selectionArgs, @Nullable String sortOrder) {
-        return null;
+        final SQLiteDatabase db = mPostDbHelper.getReadableDatabase();
+        int match = sUriMatcher.match(uri);
+
+        Cursor retCursor;
+        
+        switch (match) {
+            case MESSAGE:
+                retCursor =  db.query(TABLE_NAME,
+                        projection,
+                        selection,
+                        selectionArgs,
+                        null,
+                        null,
+                        sortOrder);
+                break;
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+        retCursor.setNotificationUri(getContext().getContentResolver(), uri);
+        
+        return retCursor;
     }
 
     @Nullable
@@ -32,7 +57,25 @@ public class MessageProvider extends ContentProvider {
     @Nullable
     @Override
     public Uri insert(@NonNull Uri uri, @Nullable ContentValues values) {
-        return null;
+        // COMPLETED (2) Write URI matching code to identify the match for the tasks directory
+        int match = sUriMatcher.match(uri);
+        Uri returnUri; // URI to be returned
+        final SQLiteDatabase db = mPostDbHelper.getWritableDatabase();
+        switch (match) {
+            case MESSAGE:
+                long id = db.insert(TABLE_NAME, null, values);
+                if ( id > 0 ) {
+                    returnUri = ContentUris.withAppendedId(MessageCPContract.MessageEntry.CONTENT_URI, id);
+                } else {
+                    throw new android.database.SQLException("Failed to insert row into " + uri);
+                }
+                break;
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+        getContext().getContentResolver().notifyChange(uri, null);
+
+        return returnUri;
     }
 
     @Override
@@ -43,5 +86,19 @@ public class MessageProvider extends ContentProvider {
     @Override
     public int update(@NonNull Uri uri, @Nullable ContentValues values, @Nullable String selection, @Nullable String[] selectionArgs) {
         return 0;
+    }
+
+    public static final int MESSAGE = 100;
+    public static final int POST_WITH_ID = 101;
+
+    private static final UriMatcher sUriMatcher = buildURIMatcher();
+
+    public static UriMatcher buildURIMatcher(){
+        UriMatcher uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+        uriMatcher.addURI(MessageCPContract.AUTHORITY,MessageCPContract.PATH_MESSAGE, MESSAGE);
+        uriMatcher.addURI(MessageCPContract.AUTHORITY,
+                MessageCPContract.PATH_MESSAGE + "/#",POST_WITH_ID);
+        return uriMatcher;
     }
 }
